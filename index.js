43c0796de7a298b355b682cf7fb7087f08bec8c6
Brett Colyer <brettc911@gmail.com>
2017-07-25 19:32:41 -0400

    fixed dynamic creation of user page

--- index.js
+++ index.js
@@ -14,9 +14,21 @@ app.get("/", function(req, res) {
   res.render('home', data)
 })
 
-// app.get("/:data", function(req, res) {
-//   res.render('user', data)
-// })
+app.get("/:user", function(req, res) {
+
+  var robotName = req.params.user
+
+  for (var i = 0; i < data.users.length; i++) {
+
+    if (data.users[i].job === null) {
+      data.users[i].job = "looking for work"
+    }
+
+    if (data.users[i].name === robotName) {
+      res.render('user', data.users[i])
+    }
+  }
+})
 
 
 app.listen(3000, function() {
